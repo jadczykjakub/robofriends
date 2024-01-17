a9660bcc2855dc7335db8dc5346c85a354176497
jadj <jakub.jadczyk@innsoft.pl>
2024-01-17 14:23:18 +0100

    my commits

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import { useDispatch, useSelector, connect } from 'react-redux';
 import './App.css';
+import { setSearchField } from '../actions';
 
-
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return { 
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  };
+}
+
+
+function App(props) {
+  const [robots, setRobots] = useState([])
+
+  
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {setRobots(users)});
+  },[])
+
+
+  const {onSearchChange} = props;
+
+  console.log(props)
+
+
+
+  const filteredRobots = robots.filter(robot =>{
+    console.log(robots)
+    // console.log(robot);
+    console.log(props.searchField)
+    return robot.name.toLowerCase().includes(props.searchField.toLowerCase());
+  })
+
+  return !robots.length ?
+    <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <button onClick={()=>setCount(count+1)}>Click Me!</button>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { Provider, connect } from 'react-redux';
+import {createStore} from 'redux';
 import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 
+
+const store = createStore(searchRobots);
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}> 
+        <App />
+    </Provider>
+);
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
